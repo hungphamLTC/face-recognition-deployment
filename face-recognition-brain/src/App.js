7196cf753558efbc9a0b25b0acf04fbf02937cc9
Hung Pham <pham.quang.thanh.hung@gmail.com>
2021-01-13 15:40:31 +0200

    project files initiation

---
+++ face-recognition-brain/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navigation from './components/Navigation';
+import Logo from './components/logo/Logo';
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+      {/* 
+      <ImageLinkForm />
+      <FaceRecognition />
+       */}
+    </div>
+  );
+}
+
+export default App;
