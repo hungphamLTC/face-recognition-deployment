bc26964273aabbb6a18b52cd4e66fb077697a5c8
Hung Pham <pham.quang.thanh.hung@gmail.com>
2021-01-14 13:29:09 +0200

    define general components with css styling using tachyons, css3 pattern gallery, logo icon and particles js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
-import Navigation from './components/Navigation';
+import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesOption = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 200
+      }
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <Particles 
+        className='particles'
+        params={particlesOption}
+      />
       <Navigation />
       <Logo />
-      {/* 
+      <Rank />
       <ImageLinkForm />
+      
+      {/* 
       <FaceRecognition />
        */}
     </div>
--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './logo.css';
 const Logo = () => {
     return(
-        <div></div>
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+                <div className="Tilt-inner"> <img style={{paddingTop: '15px'}} src={brain} alt='brain icon'/> </div>
+            </Tilt>
+        </div>
     );
 }
 
