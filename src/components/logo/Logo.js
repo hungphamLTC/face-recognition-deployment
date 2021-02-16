921f3f76d4f4988a700850f88bb15561acd6e07b
hungphamLTC <pham.quang.thanh.hung@gmail.com>
2021-02-16 18:17:32 +0200

    update bugs for deployment

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import brain from './brain.png';
-import './logo.css';
+import './Logo.css';
+
 const Logo = () => {
-    return(
-        <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner"> <img style={{paddingTop: '15px'}} src={brain} alt='brain icon'/> </div>
-            </Tilt>
+  return (
+    <div className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+        <div className="Tilt-inner pa3">
+          <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
         </div>
-    );
+      </Tilt>
+    </div>
+  );
 }
 
 export default Logo;
